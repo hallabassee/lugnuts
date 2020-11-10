eeba9966bd4eecc5382766e4beedf50583e1957b
Tanya Hlabse <tifoster@hotmail.com>
2020-11-10 17:48:30 -0500

    fresh seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(
-# 	username: "Admin2",
-# 	email: "ruby2@example.com",
-# 	password: "password",
-# 	password_confirmation: "password",
-# 	admin: true,
-# 	member: false
-# )
+User.create!(
+	username: "Admin",
+	email: "admin@example.com",
+	password: "password",
+	password_confirmation: "password",
+	admin: true,
+	member: false
+)
 
 # Add users
-# 5.times do |j|
-# 	user = User.new
-# 	user.username = Faker::Name.unique.first_name
-# 	user.email = Faker::Internet.unique.email
-# 	user.password = 'password'
-# 	user.password_confirmation = 'password'
-# 	user.admin = false
-# 	user.member = true
-# 	user.avatar.attach(io: URI.open("https://picsum.photos/200/100"), filename: "#{j}_avatar.jpg")
-# 	user.save
-# end
+5.times do |j|
+	user = User.new
+	user.username = Faker::Name.unique.first_name
+	user.email = Faker::Internet.unique.email
+	user.password = 'password'
+	user.password_confirmation = 'password'
+	user.admin = false
+	user.member = true
+	user.avatar.attach(io: URI.open("https://picsum.photos/200/200"), filename: "#{j}_avatar.jpg")
+	user.save
+end
 
 # Random number generator
 psuedo_rng = Random.new
@@ -38,8 +38,8 @@ psuedo_rng = Random.new
 	category.save
 end
 
+# Add blog posts
 5.times do |i|
-    # Add blog posts
 	post = Post.new
 	post.title = Faker::Lorem.sentence(word_count: 3, random_words_to_add: 7)
 	post.body = Faker::Lorem.paragraph_by_chars(number: 1500)
