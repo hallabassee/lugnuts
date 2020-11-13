1f7d09f9f18a44c4241ac7b9a612a22e6fec56fa
Tanya Hlabse <tifoster@hotmail.com>
2020-11-12 22:19:44 -0500

    3rd times a charm

--- db/migrate/20201113005429_create_product_image_map.rb
+++ db/migrate/20201113005429_create_product_image_map.rb
@@ -2,7 +2,7 @@ class CreateProductImageMap < ActiveRecord::Migration[6.0]
   def up
     create_table :product_image_maps do |t|
       t.string :productCode, index: true, :limit => 15
-      t.foreign_key(:products, column: :productCode, primary_key: "productCode")
+      t.foreign_key(:products, column: :productCode, primary_key: "productCode",  type: :string)
       t.timestamps
     end
     # populate the table from the products table
