690329499b5c38d52cc6db4fdb0befc9f0f93e7e
Tanya Hlabse <tifoster@hotmail.com>
2020-11-12 22:10:57 -0500

    varchar mismatch

--- db/migrate/20201113005429_create_product_image_map.rb
+++ db/migrate/20201113005429_create_product_image_map.rb
@@ -2,7 +2,7 @@ class CreateProductImageMap < ActiveRecord::Migration[6.0]
   def up
     create_table :product_image_maps do |t|
       t.string :productCode, index: true
-      t.foreign_key(:products, column: :productCode, primary_key: "productCode")
+      t.foreign_key(:products, column: :productCode, primary_key: "productCode", :limit => 15)
       t.timestamps
     end
     # populate the table from the products table
