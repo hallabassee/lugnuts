7d9e11be6f056107ad9e40b7ef2dc4641301731e
Tanya Hlabse <tifoster@hotmail.com>
2020-12-03 23:04:43 -0500

    handle nil or 0 value for posts and products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
     # GET /home
     def index
         @featured_post = Post.where(:featured => true).first
-        @latest_posts = Post.where(:featured => nil).last(3)
+        @latest_posts = Post.where(:featured => 0).or(Post.where(:featured => nil)).last(3)
         @featured_product = Product.where(:featured => true).first
-        @latest_products = Product.where(:featured => nil).last(4)
+        @latest_products = Product.where(:featured => 0).or(Product.where(:featured => nil)).last(4)
         @subscriber = Subscriber.new
     end  
    
