3020c458d9b15c157fa885b15b54288a16984a26
Tanya Hlabse <tifoster@hotmail.com>
2020-12-16 22:15:02 -0500

    corrected foreign key

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
     self.primary_key = 'orderNumber'
     belongs_to :customer, :foreign_key => :customerNumber
-    has_many :orderdetails, :foreign_key => [:orderNumber, :productCode], dependent: :destroy
+    has_many :orderdetails, :foreign_key => :orderNumber, dependent: :destroy
 end
