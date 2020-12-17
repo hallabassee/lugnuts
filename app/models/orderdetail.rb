a3bdfd9beba85d16fd4bb5cb3344848e7f087813
Tanya Hlabse <tifoster@hotmail.com>
2020-12-16 22:22:43 -0500

    Primary key correction

--- app/models/orderdetail.rb
+++ app/models/orderdetail.rb
@@ -1,5 +1,5 @@
 class Orderdetail < ApplicationRecord
-    self.primary_keys = :orderNumber, :productCode
+    self.primary_key = :orderNumber
     belongs_to :order, :foreign_key => "orderNumber"
     belongs_to :product, :foreign_key => "productCode"
 end
